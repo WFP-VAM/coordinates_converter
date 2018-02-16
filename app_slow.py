e99eb73c439340feb67ac64f111413b7f95934d1
lorenzo.riches <lorenzo.riches@wfp.org>
2018-02-16 14:55:50 +0100

    using geopandas.

--- app_slow.py
+++ app_slow.py
@@ -1,12 +1,27 @@
 # https://stackoverflow.com/questions/7861196/check-if-a-geopoint-with-latitude-and-longitude-is-within-a-shapefile
+import shapefile
 import os
 import pandas as pd
-from shapely.geometry import Point
+from shapely.geometry import shape, Point
 from flask import Flask, request, render_template, make_response
 from werkzeug.utils import secure_filename
-import geopandas as gp
-from geopandas.tools import sjoin
 app = Flask(__name__, instance_relative_config=True)
+
+
+def converter_func(shapefile, lat, lon):
+
+    for shp in shapefile.shapeRecords():
+        # build a shapely polygon from your shape
+        polygon = shape(shp.shape)
+
+        point = Point(lon, lat)
+
+        if polygon.contains(point):
+            print('bingo! -> {}'.format(shp.record))
+            return shp.record
+
+    print('cooridnates not in shapefile. Maybe at sea?')
+    return [None, None, None, None]
 
 
 @app.route('/')
@@ -24,25 +39,25 @@ def covert():
     coordinates = request.files['file']
     df = pd.read_csv(coordinates)
     print(df.head())
-    geometry = [Point(xy) for xy in zip(df.Longitude, df.Latitude)]
-    crs = {'init': 'epsg:2263'}  # http://www.spatialreference.org/ref/epsg/2263/
-    geo_df = gp.GeoDataFrame(df, crs=crs, geometry=geometry)
 
     # shapefile
     if 'shfile' not in request.files:
         # use local shapefile
-        shpf = gp.GeoDataFrame.from_file('data/global_adm2.shp')
+        shapefile_path = 'data/global_adm1.shp'
+        shpf = shapefile.Reader(shapefile_path)
     else:
         shfile = request.files['shfile']
         filename = secure_filename(shfile.filename)
         shfile.save('data/'+filename)
-        shpf = gp.GeoDataFrame.from_file('data/'+filename)
+        shpf = shapefile.Reader('data/'+filename)
 
-    pointInPolys = sjoin(geo_df, shpf, how='left')
+    for ix, row in df.iterrows():
+        print('finding coordinates {} {}'.format(row.Latitude, row.Longitude))
+        df.loc[ix, 'ADM{}_ID'.format(adm_level)], df.loc[ix, 'ADM{}'.format(adm_level)], \
+        df.loc[ix, 'ADM0_ID'], df.loc[ix, 'ADM0'] =\
+            converter_func(shpf, row.Latitude, row.Longitude)
 
-    output = pd.DataFrame(pointInPolys.drop('geometry', axis=1))
-
-    resp = make_response(output.to_csv())
+    resp = make_response(df.to_csv())
     resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
     resp.headers["Content-Type"] = "text/csv"
     return resp
