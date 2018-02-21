59e6d03b544d46df823d2a69fb3d141f372f60c4
lorenzo.riches <lorenzo.riches@wfp.org>
2018-02-21 09:13:26 +0100

    changed to port 2018

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ if __name__ == '__main__':
             "please wait until server has fully started"))
 
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 2018))
     app.run(host='0.0.0.0', port=port)
